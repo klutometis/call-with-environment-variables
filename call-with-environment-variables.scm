600d1c01c5b3a73edbac2c5476f793a214d9833b
Peter Danenberg <pcd@roxygen.org>
2012-09-26 15:15:27 -0700

    Prepare for docs.

--- call-with-environment-variables.scm
+++ call-with-environment-variables.scm
@@ -4,25 +4,4 @@
 
  (import scheme chicken)
 
- (define (call-with-environment-variables variables thunk)
-  (let ((pre-existing-variables
-         (map (lambda (var-value)
-                (let ((var (car var-value)))
-                  (cons var (get-environment-variable var))))
-              variables)))
-    (dynamic-wind
-        (lambda () (void))
-        (lambda ()
-          (use posix)
-          (for-each (lambda (var-value)
-                      (setenv (car var-value) (cdr var-value)))
-                    variables)
-          (thunk))
-        (lambda ()
-          (for-each (lambda (var-value)
-                      (let ((var (car var-value))
-                            (value (cdr var-value)))
-                        (if value
-                            (setenv var value)
-                            (unsetenv var))))
-                    pre-existing-variables))))))
+ (include "call-with-environment-variables-core.scm"))
